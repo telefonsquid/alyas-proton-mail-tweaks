495e523cd0dc338ac3c32d08edbc389b8663f225
Alya Henkys <alya@henkys.dev>
2026-09-19 15:41:53 +0200

    updated default port

--- deploy/unraid-update.sh
+++ deploy/unraid-update.sh
@@ -7,7 +7,7 @@ REPO_DIR="/mnt/user/appdata/proton-mail-tweaks"
 LOG_FILE="$REPO_DIR/deploy.log"
 IMAGE="proton-mail-tweaks"
 CONTAINER="proton-mail-tweaks"
-PORT="9030"
+PORT="9930"
 ORIGIN="https://proton-mail-tweaks.henkys.dev"
 
 git config --global --add safe.directory $REPO_DIR
